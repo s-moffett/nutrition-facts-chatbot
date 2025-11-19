6d35edbb915dd9fb09f27c633ddb57e0e89936c5
Seth Moffett <moffett.seth@gmail.com>
2025-11-19 12:18:11 -0600

    Reorganize code

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A small RAG project that uses an LLM to answer nutrition questions based on the 
 Make sure an Anthropic API key is defined in `ANTHROPIC_API_KEY`.
 I recommend using a venv with Python 3.12.
 
+Unzip the nutrition_facts_data into data/.
+
 `pip install -r requirements.txt`
 
 `python src/main.py`
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from vector_db.vector_db import VectorDB
 from llm_client.claude_client import ClaudeClient
 from llm_client.llm_client import LLMClient
 
+import textwrap
+
 def answer_user_question(llm: LLMClient, db: VectorDB):
     user_query = input('Ask a nutrition question: ')
     print('\n...\n')
@@ -15,15 +17,29 @@ def answer_user_question(llm: LLMClient, db: VectorDB):
     return
 
 def generate_rag_query(llm: LLMClient, query: str) -> str:
-    rag_query_prompt = f'The user is searching a vector database that contains nutrition blog posts. Add useful synonyms and keywords to their query, and remove unnecessary words. \n\nUser query: {query}'
-    return llm.message(rag_query_prompt)
+    rag_prompt = build_markdown_llm_prompt({
+        "Instructions": "I am searching for nutrition information in a vector database that contains nutrition-related blog posts. Add useful synonyms and keywords to my query, and remove unnecessary words.",
+        "Query": query
+    })
+
+    return llm.message(rag_prompt)
 
 def get_context(db: VectorDB, query: str) -> list[BlogPost]:
     return db.query_db(query)
 
 def answer_query_with_context(llm: LLMClient, query: str, context: list[BlogPost]) -> str:
-    blogs_as_text = convert_blogs_to_text(context)
-    prompt = f"Answer the user's question about nutrition based on the included blog content. Cite sources as much as possible by including the relevant blog post URL. User question: {query}\n\nBlog content:\n\n{blogs_as_text}"
+    CHAT_INSTRUCTIONS = textwrap.dedent("""
+        - You are an AI assistant that answers nutrition-related questions.
+        - Use the provided blog content to inform your answers.
+        - Cite sources by including the relevant blog post URL in your answers.
+    """)
+
+    prompt = build_markdown_llm_prompt({
+        "Instructions": CHAT_INSTRUCTIONS,
+        "Question": query,
+        "Blog Content": convert_blogs_to_text(context)
+    })
+
     response = llm.message(prompt)
     return response
 
@@ -31,6 +47,10 @@ def convert_blogs_to_text(blog_posts: list[BlogPost]) -> str:
     blog_separator = "\n---\n"
     formatted_blogs = [f"Content:\n{blog.content}\nURL:\n{blog.url}" for blog in blog_posts]
     return blog_separator.join(formatted_blogs)
+
+def build_markdown_llm_prompt(elements: dict) -> str:
+    sections = [f"# {title}\n{content}" for title, content in elements.items()]
+    return "\n\n".join(sections)
 
 llm = ClaudeClient()
 db = ChromaDB()
